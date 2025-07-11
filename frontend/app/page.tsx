d8e4ac591caa3ae63a06104222a8e9c0e0e51aa2
Dominic Chen <DChen@fgfbrands.com>
2025-07-11 10:11:50 -0400

    INITIAL COMMIT

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-dark w-screen">
+      <h1 className="text-fgf">HELLO</h1>
+    </div>
+  );
+}
